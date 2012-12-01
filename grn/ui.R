1464f1c9adbeee96135b7e395fb59e2f3b85f5a4
trestletech <jeff.allen@trestletechnology.net>
2012-12-01 00:12:30 -0600

    Rearranged UI with sidebar panel.

--- grn/ui.R
+++ grn/ui.R
@@ -1,19 +1,24 @@
-shinyUI(bootstrapPage(
-
-  selectInput(inputId = "method",
-      label = "Method to use to reconstruct the network:",
-      choices = c("GeneNet"),
-      selected = "GeneNet"),
-
-  fileInput(inputId = "file", label="Network to reconstruct:"),
+shinyUI(pageWithSidebar(
+  headerPanel("Reconstruct Gene Networks"),
   
-  plotOutput(outputId = "main_plot", height = "500px"),
-
-  verbatimTextOutput("debug"),
+  sidebarPanel(
+    fileInput(inputId = "file", label="Network to reconstruct:"),
+    
+    selectInput(inputId = "method",
+        label = "Method to use to reconstruct the network:",
+        choices = c("GeneNet"),
+        selected = "GeneNet"),
   
-  sliderInput(inputId = "con_weight",
-        label = "Connection threshold:",
-        min = 0.0, max = 1, value = .25, step = 0.05)
+    sliderInput(inputId = "con_weight",
+                label = "Connection threshold:",
+                min = 0.0, max = 1, value = .25, step = 0.05)
+  ),
+  
+  mainPanel(    
+    plotOutput(outputId = "main_plot", height = "500px"),
+    verbatimTextOutput("debug")
+  )
+  
   
 
 ))
