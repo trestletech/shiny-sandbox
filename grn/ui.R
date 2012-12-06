09ad7d0c5594ffd0cd1c8b762086e1577e068d97
trestletech <jeff.allen@trestletechnology.net>
2012-12-06 17:54:14 +0000

    Added Google Analytics.

--- grn/ui.R
+++ grn/ui.R
@@ -4,6 +4,23 @@ reactiveNetwork <- function (outputId)
 }
 
 
+googleAnalytics <- function(account="UA-36850640-1"){
+  HTML(paste("<script type=\"text/javascript\">
+
+    var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', '",account,"']);
+  _gaq.push(['_setDomainName', 'rstudio.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+  </script>", sep=""))
+}
+
 
 shinyUI(pageWithSidebar(    
   headerPanel("Reconstruct Gene Networks"),
@@ -63,7 +80,8 @@ helpText("Use the slider to set the number of connections which
   
   mainPanel(    
     includeHTML("graph.js"),
-    reactiveNetwork(outputId = "mainnet")    
+    reactiveNetwork(outputId = "mainnet") ,
+    googleAnalytics()
   )
   
   
